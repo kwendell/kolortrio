ef11d1a04eae90936eac8ab07083ed7b42f16c8c
Kevin Duell <kdubzot9@gmail.com>
2015-12-30 14:57:51 -0800

    Set states on play song event.

--- js/recordPlayerView.js
+++ js/recordPlayerView.js
@@ -1,4 +1,23 @@
 var RecordPlayerView = function() {
   this.name = ko.observable("Record Player");
+  var self = this;
+  this.DEFAULT="DEFAULT";
+  this.PLAY="PLAY";
+  this.viewState = self.DEFAULT;
+  this.playSong = function(data,event) {
+    //console.log(event.toElement.id);
+    //console.log(self.getState());
+    self.viewState = self.PLAY;
+    //console.log(self.getState());
+
+
+  };
+
+
+  this.getState = function() {
+  	return this.viewState;
+  };
+
+
 
 };
