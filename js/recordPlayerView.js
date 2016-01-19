d6e8c3cab629e2703e909b1c2f5cfb7d2502b49c
kevind <kevind@altierre.com>
2016-01-19 11:35:10 -0800

    requesting animation frame.

--- js/recordPlayerView.js
+++ js/recordPlayerView.js
@@ -17,7 +17,38 @@ var RecordPlayerView = function() {
   this.getState = function() {
   	return this.viewState;
   };
+  
+  this.updateView = function() {
+  //alert("updateView::method");
+   
+  };
+    var ONE_FRAME_TIME = 1000 / 60 ;
+  var mainloop  = function(global) {
+   
+	
+	  self.updateView();
+	
+	  
+	};
+	  var animFrame = window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
+            null ;
+   
+var recursiveAnim = function() {
+        mainloop();
+        animFrame( recursiveAnim );
+    };
 
+    // start the mainloop
+    animFrame( recursiveAnim );
+//	setInterval(mainloop, 1000);
 
+/** Syntax
+ * var Engine = (function(global) {
+ * })(this);
+ */
 
 };
